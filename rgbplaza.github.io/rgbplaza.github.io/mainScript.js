c86935f5d9a8c58b336048b4475bc19f00e9e5d9
RGBPlaza <rayred14@live.com>
2016-11-05 15:31:26 +0000

    started sections and made section menu

--- rgbplaza.github.io/rgbplaza.github.io/mainScript.js
+++ rgbplaza.github.io/rgbplaza.github.io/mainScript.js
@@ -1,3 +1,26 @@
 ﻿$(document).ready(function () {
-    $("#mainSlider").rgbslide();
+    $("#mainSlider").rgbslide(3550);
+
+    $(".topBtn").click(function () {
+        $("body").animate({ scrollTop: "0px"});
+    });
+    $(".appBtn").click(function () {
+        $("body").animate({ scrollTop: $("#apps").offset().top + "px" });
+    });
+    $(".webBtn").click(function () {
+        $("body").animate({ scrollTop: $("#web").offset().top + "px" });
+    });
+    $(".creBtn").click(function () {
+        $("body").animate({ scrollTop: $("#creations").offset().top + "px" });
+    });
+
+    $("#sectionMenu").css("height", $("body").height() + "px");
+    $(".sect h1, #navBar h1").click(function () {
+        $("#sectionMenu").fadeIn();
+        $("body").css("overflow-y", "hidden");
+    });
+    $("#sectionMenu h1").click(function () {
+        $("#sectionMenu").fadeOut();
+        $("body").css("overflow-y", "auto");
+    });
 });
