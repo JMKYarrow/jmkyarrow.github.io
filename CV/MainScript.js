b318da06fe4b4bf26d5e348e13487baee5a06d60
RGBPlaza <rayred14@live.com>
2017-09-30 15:28:36 +0100

    GCSE Predicted Grades & Other achievements

--- CV/MainScript.js
+++ CV/MainScript.js
@@ -2,6 +2,12 @@
 
     var selectedButton = "#navBtn0";
 
+    var grades = [["<span style='font-size:24px'>Subject</span>", "<span style='font-size:24px'>Grade</span>"], ["Biology", "9"], ["Chemistry", "9"], ["Computer Sci.", "8"], ["English Lit.", "8"], ["English Lang.", "8"], ["French", "8"], ["Maths", "9"], ["Music", "8"], ["Physics", "9"], ["R.S.", "8"],]
+
+    for (var i = 0; i < grades.length; i++){
+        $("#gradesTable").append('<div><div class="cell col0" style="top:' + (46 * (i + 1)) + 'px;">' + grades[i][0] + '</div><div class="cell col1" style="top:' + (46 * (i + 1)) + 'px;">' + grades[i][1] + '</div></div>')
+    }
+
     $("#navIndicator").css("top", $(selectedButton).css("height"));
     $(".pane").css("top", $(selectedButton).css("height"));
     updateNavIndicator(selectedButton);
@@ -13,6 +19,16 @@
         selectedButton = "#" + $(this).prop("id");
         updateNavIndicator(selectedButton);
         updatePaneArea(selectedButton);
+    });
+
+    $("#viewGradesButton").click(function () {
+        $("#gradesModal").css("top", 0);
+        $("body").css("overflow-y", "hidden");
+    });
+
+    $(".modalCloseButton").click(function () {
+        $("#gradesModal").css("top", "-100vh");
+        $("body").css("overflow-y", "auto");
     });
 
 });
