10e135dbc20a1ca5fa6d693bc440f36b28eb9f0d
RGBPlaza <rayred14@live.com>
2017-09-30 01:42:11 +0100

    Started Work on CV

---
+++ CV/MainScript.js
@@ -0,0 +1,28 @@
+﻿$(document).ready(function () {
+
+    var selectedButton = "#navBtn0";
+
+    $("#navIndicator").css("top", $(selectedButton).css("height"));
+    $(".pane").css("top", $(selectedButton).css("height"));
+    updateNavIndicator(selectedButton);
+    window.onresize = function () {
+        updateNavIndicator(selectedButton);
+    }
+
+    $(".navButton").click(function () {
+        selectedButton = "#" + $(this).prop("id");
+        updateNavIndicator(selectedButton);
+        updatePaneArea(selectedButton);
+    });
+
+});
+
+function updateNavIndicator(buttonID) {
+    $("#navIndicator").css("width", $(buttonID).css("width"));
+    $("#navIndicator").css("left", $(buttonID).position().left);
+}
+
+function updatePaneArea(buttonID) {
+    $(".pane").removeClass("paneIsVisible");
+    $(buttonID.replace("navBtn", "pane")).addClass("paneIsVisible");
+}
