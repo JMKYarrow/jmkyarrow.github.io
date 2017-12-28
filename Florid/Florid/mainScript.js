c8e45793dc73990f8546b91826219cf29f4990f6
RGBPlaza <rayred14@live.com>
2017-12-28 00:19:59 +0000

    First Upload

---
+++ Florid/Florid/mainScript.js
@@ -0,0 +1,10 @@
+﻿/*window.onload = function () {
+    var shadowIsAtTop = false;
+    setInterval(function () {
+        if (shadowIsAtTop)
+            document.getElementById("titleShadow").style.top = "34px";
+        else
+            document.getElementById("titleShadow").style.top = "30px";
+        shadowIsAtTop = !shadowIsAtTop;
+    }, 200);
+}*/
